dc04ef8518e329584a48e16fb12aaf1616812109
imrancoder10 <imranhossain1148@gmail.com>
2024-09-07 22:33:09 +0600

    celsius to fahrenheit

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+/**
+ * 
+### Task-1: 
+Write a function to convert temperature from Celsius to Fahrenheit.
+ */
+const celsius = 120;
+const fahrenheit = (9 / 5)* celsius + 32;
+console.log(fahrenheit);
